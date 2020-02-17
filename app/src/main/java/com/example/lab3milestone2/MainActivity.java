7eeebced6605dbf5fb3c56673ebc6f8e2e085725
kshen26@wisc.edu <kshen26@wisc.edu>
2020-02-17 14:42:43 -0600

    m2

--- app/src/main/java/com/example/lab3milestone2/MainActivity.java
+++ app/src/main/java/com/example/lab3milestone2/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.lab3milestone2;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.abhiandroid.fragmentexample.R;
 
 public class MainActivity extends AppCompatActivity {
+    Button firstFragment, secondFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firstFragment = (Button) findViewById(R.id.firstFragment);
+        secondFragment = (Button) findViewById(R.id.secondFragment);
+
+        firstFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FirstFragment());
+            }
+        });
+        secondFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new SecondFragment());
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
